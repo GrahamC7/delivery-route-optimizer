ff33b2ce555eebc6ce58a04f90c2c8704380b614
Graham <scocker@wgu.edu>
2025-02-13 19:12:59 -0500

    modified Main.py code - bug fixes

--- Main.py
+++ Main.py
@@ -27,39 +27,42 @@ class HashTable:
             self.table.append([])
 
         # inserting new item into hash table
-        def insert(self, key, item):
-            bucket = hash(key) % len(self.table)
-            bucket_list = self.table[bucket]
 
-            # updating existing keys in bucket
-            for kv in bucket_list:
-                if kv[0] == key:
-                    kv[1] = item
-                    # printing key value
-                    return True
+    def insert(self, key, item):
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
 
-            # inserting item into the end of the list if it does not already exist
-            key_value = [key, item]
-            bucket_list.append(key_value)
-            return True
+        # updating existing keys in bucket
+        for kv in bucket_list:
+            if kv[0] == key:
+                kv[1] = item
+                # printing key value
+                return True
+
+        # inserting item into the end of the list if it does not already exist
+        key_value = [key, item]
+        bucket_list.append(key_value)
+        return True
 
         # searching hash table for item that matches the key
-        def search(self, key):
-            bucket = hash(key) % len(self.table)
-            bucket_list = self.table[bucket]
 
-            for kv in bucket_list:
-                if kv[0] == key:
-                    return kv[1]  # if item is found
-            return None  # if item is not found
+    def search(self, key):
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+
+        for kv in bucket_list:
+            if kv[0] == key:
+                return kv[1]  # if item is found
+        return None  # if item is not found
 
         # removal of item from hash table
-        def hash_remove(self, key):
-            bucket = hash(key) % len(self.table)
-            bucket_list = self.table[bucket]
-            # removal of item that matches hash table key
-            if key in bucket_list:
-                bucket_list.remove(key)
+
+    def hash_remove(self, key):
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+        # removal of item that matches hash table key
+        if key in bucket_list:
+            bucket_list.remove(key)
 
 
 # creation of PackageFunction class - stores information about the packages
@@ -131,43 +134,44 @@ def load_packages(filename, packages_hash):
 package_hash = HashTable()
 
 
-# creating class for delivery trucks
-class Trucks:
-    def __init__(self, speed, miles, packages, current_location, departure_time):
-        self.speed = speed
-        self.miles = miles
-        self.packages = packages
-        self.current_location = current_location
-        self.departure_time = departure_time
-        self.time = departure_time
-
-    def __str__(self):
-        return "%s, %s, %s, %s, %s, %s" % (
-            self.speed, self.miles, self.packages, self.current_location, self.departure_time, self.time)
-
-
 # finding the shortest distance to next delivery address
 def address(address_main):
-    for row in addressCSV:
+    for row in AddressCSV:
         if address_main in row[2]:
             return int(row[0])
 
 
 # comparing distance between two addresses
 def between_address(address1, address2):
-    distance = distanceCSV[address1][address2]
+    distance = DistanceCSV[address1][address2]
     if distance == '':
-        distance = distanceCSV[address2][address1]
+        distance = DistanceCSV[address2][address1]
     return float(distance)
 
 
 def address_info(address_main):
-    for row in addressCSV:
+    for row in AddressCSV:
         if address_main in row[2]:
             return int(row[0])
 
 
 load_packages("CSV_Files/packages.csv", package_hash)
+
+
+# creating class for delivery trucks
+class Trucks:
+    def __init__(self, speed, miles, current_location, departure_time, packages):
+        self.speed = speed
+        self.miles = miles
+        self.current_location = current_location
+        self.departure_time = departure_time
+        self.time = departure_time
+        self.packages = packages  # Correctly assign packages as a list
+
+    def __str__(self):
+        return "%s, %s, %s, %s, %s, %s" % (
+            self.speed, self.miles, self.current_location, self.departure_time, self.time, self.packages)
+
 
 # manually loading the trucks and assigning them a departure time
 truck1 = Trucks(18, 0.0, "4001 South 700 East", datetime.timedelta(hours=8),
@@ -186,7 +190,7 @@ def package_delivery(truck):
         package = package_hash.search(package_id)
         not_delivered.append(package)
 
-    # clearing package list to load packages in trucks using nearest neighbor algorith
+    # clearing package list to load packages in trucks using nearest neighbor algorithm
     truck.packages.clear()
 
     # cycles list of packages not yet delivered until all packages are assigned
@@ -225,13 +229,28 @@ print("Total mileage driven is: ", truck1.miles + truck2.miles + truck3.miles)
 while True:
     # printing combined mileage for the 3 trucks
     time_request = input("To see a time status update of each package, enter the time in HH:MM format.")
-    (h, m) = time_request.split(":")
-    timeChange = datetime.timedelta(hours=int(h), minutes=int(m))
+
+    # input format validation
+    if ":" not in time_request:
+        print("Invalid time format. Please use HH:MM format.")
+        continue  # Prompt again
+
+    try:
+        (h, m) = time_request.split(":")
+        timeChange = datetime.timedelta(hours=int(h), minutes=int(m))
+    except ValueError:
+        print("Invalid time format. Make sure to use numeric values in HH:MM format.")
+        continue  # Prompt again
+
     try:
         user_entry = [int(input("Enter package ID."))]
     except ValueError:
         user_entry = range(1, 41)
+
     for package_id in user_entry:
         package = package_hash.search(package_id)
-        package.status(timeChange)
-        print(str(package))
+        if package is not None:  # only updating status if the package exists
+            package.update_status(timeChange)
+            print(str(package))
+        else:
+            print(f"Package ID {package_id} not found in the hash table.")
